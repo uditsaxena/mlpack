efba7572f98d65e6c32dd5b99fe78b5bbe7e7d78
Udit Saxena <saxena.udit@gmail.com>
2014-06-08 14:51:33 +0530

    Added bucket parameter in main.cpp

--- weak_learner/decision_stump/decision_stump_main.cpp
+++ weak_learner/decision_stump/decision_stump_main.cpp
@@ -13,18 +13,22 @@ using namespace mlpack::decision_stump;
 using namespace std;
 using namespace arma;
 
-PROGRAM_INFO("","");
+PROGRAM_INFO("Decision Stump","This program implements a decision stump, "
+    "a single level decision tree, on the given training data set. "
+    "Default size of buckets is 6");
 
 // necessary parameters
-PARAM_STRING_REQ("train_file", "A file containing the training set.", "t");
+PARAM_STRING_REQ("train_file", "A file containing the training set.", "tr");
 PARAM_STRING_REQ("labels_file", "A file containing labels for the training set.",
   "l");
-PARAM_STRING_REQ("test_file", "A file containing the test set.", "T");
+PARAM_STRING_REQ("test_file", "A file containing the test set.", "te");
 PARAM_STRING_REQ("num_classes","The number of classes","c");
 
 // output parameters (optional)
 PARAM_STRING("output", "The file in which the predicted labels for the test set"
     " will be written.", "o", "output.csv");
+
+PARAM_INT("bucket_size","The size of ranges/buckets to be used while splitting the decision stump.","b", 6);
 
 int main(int argc, char *argv[])
 {
@@ -47,8 +51,7 @@ int main(int argc, char *argv[])
   if (labelsIn.n_rows == 1)
     labelsIn = labelsIn.t();
 
-  // test the dimensionality of labels and training data as well
-  /*Code goes here*/
+  size_t inpBucketSize = CLI::GetParam<int>("bucket_size");
   
   // normalize the labels
   data::NormalizeLabels(labelsIn.unsafe_col(0), labels, mappings);
@@ -67,10 +70,8 @@ int main(int argc, char *argv[])
         << "must be the same as training data (" << trainingData.n_rows - 1
         << ")!" << std::endl;
 
-  // should labels be transposed here ? Are they being sent as column here ?
-
   Timer::Start("training");
-  DecisionStump<> ds(trainingData,labels,num_classes);
+  DecisionStump<> ds(trainingData, labels, num_classes, inpBucketSize);
   Timer::Stop("training");
 
   Row<size_t> predictedLabels(testingData.n_cols);
