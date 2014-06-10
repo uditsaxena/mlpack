eda067c804b4d890d2f52495204d1529ac1a2bca
Udit Saxena <saxena.udit@gmail.com>
2014-06-11 00:07:25 +0530

    Tests added, need to get correct results.

--- decision_stump_test.cpp
+++ decision_stump_test.cpp
@@ -39,13 +39,13 @@ BOOST_AUTO_TEST_CASE(OneClass)
   data::Load(testSet, testingData, true);
   
   DecisionStump<> ds(trainingData, labelsIn.row(0), numClasses, inpBucketSize);
-/*
+
   Row<size_t> predictedLabels(testingData.n_cols);
   ds.Classify(testingData, predictedLabels);
 
   for(int i =0; i < predictedLabels.size(); i++ )
     BOOST_CHECK_EQUAL(predictedLabels(i),1);  
-*/
+
 } 
 
 BOOST_AUTO_TEST_CASE(EntropyCalculation)
@@ -73,8 +73,115 @@ BOOST_AUTO_TEST_CASE(EntropyCalculation)
   ds.Classify(testingData, predictedLabels);
   
   data::Save(output, predictedLabels, true, true);
+}
+
+BOOST_AUTO_TEST_CASE(PerfectSplitOnZero)
+{
+  const char* trainSet = "ds_trainSet4.csv";
+  const char* trainlabels = "ds_trainSetLabels4.csv";
+  const char* testSet = "ds_testSet4.csv";
+  size_t numClasses = 2;
+  const char* output = "ds_output4.csv";
+  size_t inpBucketSize = 2;
+
+  mat trainingData;
+  data::Load(trainSet, trainingData, true);
   
+  Mat<size_t> labelsIn;
+  data::Load(trainlabels, labelsIn, true);
+  // no need to normalize labels here.
+
+  mat testingData;
+  data::Load(testSet, testingData, true);
   
+  DecisionStump<> ds(trainingData, labelsIn.row(0), numClasses, inpBucketSize);
+
+  Row<size_t> predictedLabels(testingData.n_cols);
+  ds.Classify(testingData, predictedLabels);
+
+  data::Save(output, predictedLabels, true, true);
+}
+
+BOOST_AUTO_TEST_CASE(BinningTesting)
+{
+  const char* trainSet = "ds_trainSet5.csv";
+  const char* trainlabels = "ds_trainSetLabels5.csv";
+  const char* testSet = "ds_testSet5.csv";
+  size_t numClasses = 2;
+  const char* output = "ds_output5.csv";
+  size_t inpBucketSize = 10;
+
+  mat trainingData;
+  data::Load(trainSet, trainingData, true);
+  
+  Mat<size_t> labelsIn;
+  data::Load(trainlabels, labelsIn, true);
+  // no need to normalize labels here.
+
+  mat testingData;
+  data::Load(testSet, testingData, true);
+  
+  DecisionStump<> ds(trainingData, labelsIn.row(0), numClasses, inpBucketSize);
+
+  Row<size_t> predictedLabels(testingData.n_cols);
+  ds.Classify(testingData, predictedLabels);
+
+  data::Save(output, predictedLabels, true, true);
+}
+
+
+BOOST_AUTO_TEST_CASE(PerfectMultiClassSplit)
+{
+  const char* trainSet = "ds_trainSet6.csv";
+  const char* trainlabels = "ds_trainSetLabels6.csv";
+  const char* testSet = "ds_testSet6.csv";
+  size_t numClasses = 4;
+  const char* output = "ds_output6.csv";
+  size_t inpBucketSize = 4;
+
+  mat trainingData;
+  data::Load(trainSet, trainingData, true);
+  
+  Mat<size_t> labelsIn;
+  data::Load(trainlabels, labelsIn, true);
+  // no need to normalize labels here.
+
+  mat testingData;
+  data::Load(testSet, testingData, true);
+  
+  DecisionStump<> ds(trainingData, labelsIn.row(0), numClasses, inpBucketSize);
+
+  Row<size_t> predictedLabels(testingData.n_cols);
+  ds.Classify(testingData, predictedLabels);
+
+  data::Save(output, predictedLabels, true, true);
+}
+
+BOOST_AUTO_TEST_CASE(MultiClassSplit)
+{
+  const char* trainSet = "ds_trainSet7.csv";
+  const char* trainlabels = "ds_trainSetLabels7.csv";
+  const char* testSet = "ds_testSet7.csv";
+  size_t numClasses = 3;
+  const char* output = "ds_output7.csv";
+  size_t inpBucketSize = 3;
+
+  mat trainingData;
+  data::Load(trainSet, trainingData, true);
+  
+  Mat<size_t> labelsIn;
+  data::Load(trainlabels, labelsIn, true);
+  // no need to normalize labels here.
+
+  mat testingData;
+  data::Load(testSet, testingData, true);
+  
+  DecisionStump<> ds(trainingData, labelsIn.row(0), numClasses, inpBucketSize);
+
+  Row<size_t> predictedLabels(testingData.n_cols);
+  ds.Classify(testingData, predictedLabels);
+
+  data::Save(output, predictedLabels, true, true);
 }
 
 BOOST_AUTO_TEST_SUITE_END();
