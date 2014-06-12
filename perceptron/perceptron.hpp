9ac4fb24736f160790bf56645ee53b8459120e17
Udit Saxena <saxena.udit@gmail.com>
2014-06-12 23:12:10 +0530

    Tests modified

---
+++ perceptron/perceptron.hpp
@@ -0,0 +1,18 @@
+/*
+ * @file: perceptron.hpp
+ * @author: Udit Saxena
+ *
+ *
+ * Definition of Perceptron
+ */
+
+#ifndef _MLPACK_METHODS_PERCEPTRON_HPP
+#define _MLPACK_METHODS_PERCEPTRON_HPP
+
+#include <mlpack/core.hpp>
+
+namespace mlpack {
+namespace perceptron {
+
+} // namespace perceptron
+} // namespace mlpack
